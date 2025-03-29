073d57daeeafe9885c8a3d69d4fbf627a1997f70
karasb1 <bohdankaras456@gmail.com>
2025-03-29 13:38:33 +0200

    Refactor input handling and add string to integer conversion functionality

--- ExceptionApp2/Program.cs
+++ ExceptionApp2/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace ExceptionApp2;
-
-class Program
+﻿namespace class_2
 {
-    static void Main(string[] args)
+    internal class StringToInt
     {
-        Console.WriteLine("Hello, World!");
+        private string input;
+        public StringToInt(string input)
+        {
+            this.input = input;
+        }
+        public void Convert()
+        {
+            try
+            {
+                Console.WriteLine(input.GetType());
+                int result = int.Parse(input);
+                Console.WriteLine(result);
+                Console.WriteLine(result.GetType());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? input = Console.ReadLine();
+            if (input != null)
+            {
+                StringToInt operation = new StringToInt(input);
+                operation.Convert();
+            }
+            else
+            {
+                Console.WriteLine("No input provided");
+            }
+        }
     }
 }
